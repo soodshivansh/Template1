ee2a8810f25e87ef25c80738f4ecdf98079f27bb
shivansh <soodshivansh5@gmail.com>
2023-12-19 16:06:19 +0530

    responsive change

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ function hero() {
   return (
     <>
     <div className='lg:pr-20 pr-6 lg:pl-20 pl-6'>
-        <div className='pl-10 pt-10 pr-10 lg:pb-10 w-full md:h-[60vh] h-[90vh] bg-gradient-to-r from-[#c5e1e8] to-[#f8dabf] rounded-3xl flex md:flex-row flex-col items-center justify-between'>
+        <div className='pl-10 pt-10 pr-10 lg:pb-10 w-full md:h-[60vh] bg-gradient-to-r from-[#c5e1e8] to-[#f8dabf] rounded-3xl flex md:flex-row flex-col items-center justify-between'>
             <div className='flex flex-col text-left md:gap-3 sm:gap-5 gap-3'>
                 <h1 className='font-Ubuntu sm:text-lg text-base'>Welcome to AllState</h1>
                 <p className='font-Kanit lg:text-7xl sm:text-4xl text-3xl'>Manage Your Property</p>
